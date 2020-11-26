aec017d28b41e2ddbea6c17996ff4fa53d0e5feb
Chelsea Houston <chelseahouston29@gmail.com>
2020-11-26 17:12:49 +0000

    Added Bootstrap, JS and Awesome Fonts. Page more responsive but not 100%. Message thanking user when form submitted. Log in page added.

---
+++ script.js
@@ -0,0 +1,11 @@
+function myFunction() {
+  document.getElementById("contactheader").innerHTML = "Thank you for your message.";
+  var element = document.getElementById("survey-form");
+  element.parentNode.removeChild(element);
+}
+
+function currentDate() {
+
+}
+
+/* jQuery */
